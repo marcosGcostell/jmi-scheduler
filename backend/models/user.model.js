3658df127a1af23354eaa58683c4752ece25f2e1
Marcos García Costell <marcos.g.costell@outlook.com>
2026-01-19 19:22:29 +0100

    api-user-model

---
+++ backend/models/user.model.js
@@ -0,0 +1,65 @@
+import pool from '../db/pool.js';
+
+export const getAllUsers = async () => {
+  const { rows } = await pool.query(`
+    SELECT id, email, full_name, role
+    FROM users
+    ORDER BY created_at DESC
+    `);
+
+  return rows;
+};
+
+export const getUser = async id => {
+  const { rows } = await pool.query(
+    `
+    SELECT id, email, full_name, role
+    FROM users
+    WHERE id = $1
+    `,
+    [id],
+  );
+
+  return rows;
+};
+
+export const createUser = async data => {
+  const { email, full_name, password, role } = data;
+
+  const { rows } = await pool.query(
+    `
+    INSERT INTO users (email, full_name, password_hash, role)
+    VALUES ($1, $2, $3, $4)
+    RETURNING id, email, full_name, role
+  `,
+    [email, full_name, password, role],
+  );
+
+  return rows[0];
+};
+
+export const updateUser = async (id, data) => {
+  const { email, full_name, role, active } = data;
+
+  const { rows } = await pool.query(
+    `
+    UPDATE users
+    SET email = $1, full_name = $2, role = $3, active = $4
+    WHERE id = $5
+    RETURNING id, email, full_name, role
+    `,
+    [email, full_name, role, active, id],
+  );
+};
+
+export const disableUser = async id => {
+  const { rows } = await pool.query(
+    `
+    UPDATE users
+    SET active = false
+    WHERE id = $5
+    RETURNING id, email, full_name, role
+    `,
+    [email, full_name, role, active, id],
+  );
+};
